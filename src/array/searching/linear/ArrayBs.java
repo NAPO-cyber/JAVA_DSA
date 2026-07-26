5a21b39635c14d8f068903a49cf4b462f829c33d
NAPO-cyber <gopalverma2503@gmail.com>
2026-07-26 11:35:19 +0530

    arr binary search

---
+++ src/array/searching/linear/ArrayBs.java
@@ -0,0 +1,30 @@
+package array.searching.linear;
+
+// Keeps dividing the array in half. (divide & conquer)...
+// works on sorted arrays only.
+
+public class ArrayBs {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,30,40,50,60,70};
+        int target = 50;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left+right)/2;
+
+            if (arr[mid] == target) {
+                index = mid;
+                break;
+            } else if (target < arr[mid]) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        System.out.println("target at: " + index);
+    }
+}
